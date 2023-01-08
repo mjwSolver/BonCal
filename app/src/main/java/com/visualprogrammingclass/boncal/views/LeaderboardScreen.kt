ec9076b0c26dd4897dfdd2e4aa73abf30de802a0
mjwSolver <mwiradinata@student.ciputra.ac.id>
2023-01-08 19:20:00 +0700

    Leaderboard
    Added Padding to allow clean viewing.
    Added theme awareness

--- app/src/main/java/com/visualprogrammingclass/boncal/views/LeaderboardScreen.kt
+++ app/src/main/java/com/visualprogrammingclass/boncal/views/LeaderboardScreen.kt
@@ -3,9 +3,7 @@ package com.visualprogrammingclass.boncal.views
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.TopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -17,11 +15,14 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.visualprogrammingclass.boncal.viewModels.LeaderboardViewModel
 import com.visualprogrammingclass.boncal.views.ui.theme.Slate900
+import com.visualprogrammingclass.boncal.views.ui.theme.backgroundColor
+import com.visualprogrammingclass.boncal.views.ui.theme.foregroundColor
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -45,8 +46,8 @@ fun LeaderboardScreen(
     }
 
     Scaffold(
-        topBar = { TopAppBar(title = { Text("BonCal", color = Color.White) },
-            backgroundColor = Slate900
+        topBar = { TopAppBar(title = { Text("BonCal", color = foregroundColor()) },
+            backgroundColor = backgroundColor()
         ) },
         content = {
             LeaderboardContent(theToken.value.toString())
@@ -64,16 +65,22 @@ fun LeaderboardContent(token:String){
 
         // Adding a WebView inside AndroidView
         // with layout as full screen
-        AndroidView(factory = {
-            WebView(it).apply {
-                layoutParams = ViewGroup.LayoutParams(
-                    ViewGroup.LayoutParams.MATCH_PARENT,
-                    ViewGroup.LayoutParams.MATCH_PARENT
-                )
-                webViewClient = WebViewClient()
-                loadUrl(mUrl)
-            }
-        }, update = {
-            it.loadUrl(mUrl)
-        })
+        Box(
+            modifier = Modifier.padding(bottom = 60.dp)
+        ){
+            AndroidView(factory = {
+                WebView(it).apply {
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    webViewClient = WebViewClient()
+                    loadUrl(mUrl)
+                }
+            }, update = {
+                it.loadUrl(mUrl)
+            })
+        }
+
+
     }
